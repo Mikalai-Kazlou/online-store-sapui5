f133d2f23d54724da3404c3c7dc4012250371b13
Mikalai Kazlou <mikalai_kazlou@epam.com>
2023-04-02 21:50:02 +0300

    feat: add routing

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -4,6 +4,9 @@ sap.ui.define([
   "use strict";
 
   return BaseController.extend("com.exercise.onlinestoresapui5.controller.Main", {
+    onOpenCart: function (oEvent) {
+      this.navTo("cart");
+    }
   });
 
 });
--- webapp/controller/ProductCatalog.controller.js
+++ webapp/controller/ProductCatalog.controller.js
@@ -3,7 +3,13 @@ sap.ui.define([
 ], function (BaseController) {
   "use strict";
 
-  return BaseController.extend("com.exercise.onlinestoresapui5.controller.ProductList", {
+  return BaseController.extend("com.exercise.onlinestoresapui5.controller.ProductCatalog", {
+    onOpenDetails: function (oEvent) {
+      const oItem = oEvent.getSource();
+      this.navTo("details", {
+        id: oItem.getBindingContext("products").getPath().split("/").pop()
+      });
+    }
   });
 
 });
