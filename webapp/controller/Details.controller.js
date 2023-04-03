5fe28bc600e6b6a217e3a69a09c8c3a5cbcd17ca
Mikalai Kazlou <mikalai_kazlou@epam.com>
2023-04-03 21:18:40 +0300

    feat: add product title on delails page

--- webapp/controller/Details.controller.js
+++ webapp/controller/Details.controller.js
@@ -4,6 +4,17 @@ sap.ui.define([
   "use strict";
 
   return BaseController.extend("com.exercise.onlinestoresapui5.controller.Details", {
+    onInit: function () {
+      const oRouter = this.getRouter();
+      oRouter.getRoute("details").attachPatternMatched(this._onObjectMatched, this);
+    },
+
+    _onObjectMatched: function (oEvent) {
+      this.getView().bindElement({
+        path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").id),
+        model: "products"
+      });
+    },
   });
 
 });
--- webapp/controller/ProductCatalog.controller.js
+++ webapp/controller/ProductCatalog.controller.js
@@ -1,13 +1,21 @@
 sap.ui.define([
-  "./BaseController"
-], function (BaseController) {
+  "./BaseController",
+  "sap/ui/model/json/JSONModel",
+], function (BaseController, JSONModel) {
   "use strict";
 
   return BaseController.extend("com.exercise.onlinestoresapui5.controller.ProductCatalog", {
+    onInit: function () {
+      var oViewModel = new JSONModel({
+        currency: "EUR"
+      });
+      this.getView().setModel(oViewModel, "view");
+    },
+
     onOpenDetails: function (oEvent) {
       const oItem = oEvent.getSource();
       this.navTo("details", {
-        id: oItem.getBindingContext("products").getPath().split("/").pop()
+        id: window.encodeURIComponent(oItem.getBindingContext("products").getPath().slice(1))
       });
     }
   });
