162cc0c809a1c47c056f19274c2d5ac01c9dc585
Mikalai Kazlou <mikalai_kazlou@epam.com>
2023-04-06 21:24:36 +0300

    feat: add search string

--- webapp/controller/ProductCatalog.controller.js
+++ webapp/controller/ProductCatalog.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
   "./BaseController",
-  "sap/ui/model/json/JSONModel",
-], function (BaseController, JSONModel) {
+  "sap/ui/model/Filter",
+  "sap/ui/model/FilterOperator"
+], function (BaseController, Filter, FilterOperator) {
   "use strict";
 
   return BaseController.extend("com.exercise.onlinestoresapui5.controller.ProductCatalog", {
@@ -10,6 +11,19 @@ sap.ui.define([
       this.navTo("details", {
         id: window.encodeURIComponent(oItem.getBindingContext("products").getPath().slice(1))
       });
+    },
+
+    onSearch: function (oEvent) {
+      // add filter for search
+      const aFilters = [];
+      const sQuery = oEvent.getSource().getValue();
+      if (sQuery && sQuery.length > 0) {
+        aFilters.push(new Filter("title", FilterOperator.Contains, sQuery));
+      }
+      // update list binding
+      const oList = this.byId("idProductCatalog");
+      const oBinding = oList.getBinding("items");
+      oBinding.filter(aFilters, "Application");
     }
   });
 
