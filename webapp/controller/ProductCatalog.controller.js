3f0a0ee9fcb4104e36bceb7a06b96c1924b1d707
Mikalai Kazlou <mikalai_kazlou@epam.com>
2023-04-14 22:02:33 +0300

    feat: apply category and brand filters

--- webapp/controller/ProductCatalog.controller.js
+++ webapp/controller/ProductCatalog.controller.js
@@ -1,14 +1,15 @@
 sap.ui.define([
   "./BaseController",
   "sap/ui/model/Filter",
+  "sap/ui/model/FilterType",
   "sap/ui/model/FilterOperator"
-], function (BaseController, Filter, FilterOperator) {
+], function (BaseController, Filter, FilterType, FilterOperator) {
   "use strict";
 
   return BaseController.extend("com.exercise.onlinestoresapui5.controller.ProductCatalog", {
     onInit: function () {
       const oModel = this.getOwnerComponent().getModel("mockdata");
-      oModel.attachBatchRequestCompleted(null, this._onBatchRequestCompleted.bind(this));
+      oModel.attachBatchRequestCompleted(this._onBatchRequestCompleted.bind(this));
     },
 
     _onBatchRequestCompleted(oEvent) {
@@ -36,16 +37,48 @@ sap.ui.define([
     },
 
     onSearch: function (oEvent) {
-      // add filter for search
-      const aFilters = [];
-      const sQuery = oEvent.getSource().getValue();
-      if (sQuery && sQuery.length > 0) {
-        aFilters.push(new Filter("title", FilterOperator.Contains, sQuery));
-      }
-      // update list binding
       const oList = this.byId("idProductCatalog");
       const oBinding = oList.getBinding("items");
-      oBinding.filter(aFilters, "Application");
+
+      let aFilters = oBinding.getFilters(FilterType.Application);
+      aFilters = aFilters.filter((item) => item.getPath() !== "Title");
+
+      const sQuery = oEvent.getSource().getValue();
+      if (sQuery && sQuery.length > 0) {
+        aFilters.push(new Filter("Title", FilterOperator.Contains, sQuery));
+      }
+
+      oBinding.filter(aFilters, FilterType.Application);
+    },
+
+    onFilterCategorySelectionChange: function (oEvent) {
+      const oList = this.byId("idProductCatalog");
+      const oBinding = oList.getBinding("items");
+
+      let aFilters = oBinding.getFilters(FilterType.Application);
+      aFilters = aFilters.filter((item) => item.getPath() !== "Category");
+
+      const aSelectedItems = oEvent.getSource().getSelectedItems();
+      aSelectedItems.forEach((item) => {
+        aFilters.push(new Filter("Category", FilterOperator.EQ, item.getTitle()));
+      });
+
+      oBinding.filter(aFilters, FilterType.Application);
+    },
+
+    onFilterBrandSelectionChange: function (oEvent) {
+      const oList = this.byId("idProductCatalog");
+      const oBinding = oList.getBinding("items");
+
+      let aFilters = oBinding.getFilters(FilterType.Application);
+      aFilters = aFilters.filter((item) => item.getPath() !== "Brand");
+
+      const aSelectedItems = oEvent.getSource().getSelectedItems();
+      aSelectedItems.forEach((item) => {
+        aFilters.push(new Filter("Brand", FilterOperator.EQ, item.getTitle()));
+      });
+
+      oBinding.filter(aFilters, FilterType.Application);
     }
   });
 
