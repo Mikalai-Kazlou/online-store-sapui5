5f68fdbe648353876ea561048c4c368eb6dee4bb
Mikalai Kazlou <mikalai_kazlou@epam.com>
2023-04-18 22:21:09 +0300

    feat: add cart list

--- webapp/controller/Cart.controller.js
+++ webapp/controller/Cart.controller.js
@@ -1,9 +1,35 @@
 sap.ui.define([
-  "./BaseController"
-], function (BaseController) {
+  "./BaseController",
+  "sap/m/StandardListItem"
+], function (BaseController, StandardListItem) {
   "use strict";
 
   return BaseController.extend("com.exercise.onlinestoresapui5.controller.Cart", {
+    onInit: function () {
+      const oRouter = this.getRouter();
+      oRouter.getRoute("cart").attachPatternMatched(this._onPatternMatched, this);
+    },
+
+    _onPatternMatched: function () {
+      const oLocalDataModel = this.getModel("localdata");
+      const aCartData = oLocalDataModel.getProperty("/cart");
+
+      const oCartList = this.byId("idCartList");
+      oCartList.removeAllItems();
+
+      aCartData.forEach((data) => {
+        const oCartListItem = new StandardListItem({
+          title: "{mockdata>Title}",
+          description: "{mockdata>Brand}",
+          icon: "{mockdata>Thumbnail}",
+          iconDensityAware: false,
+          iconInset: false
+        });
+
+        oCartList.addItem(oCartListItem);
+        oCartListItem.bindElement({ path: `/ZMK_C_PRODUCT(${data.id})`, model: "mockdata" });
+      });
+    },
   });
 
 });
