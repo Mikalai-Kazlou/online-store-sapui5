096176e7fe519e619d5ab4aa9a8160dc78da7f24
Mikalai Kazlou <mikalai_kazlou@epam.com>
2024-08-21 09:23:20 +0300

    bundle: 2024-08-21.1

--- model/Cart-dbg.js
+++ model/Cart-dbg.js
@@ -1,64 +1,65 @@
-sap.ui.define([
-  "sap/ui/base/Object",
-  "./constants"
-], function (Object, constants) {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/base/Object", "./constants"],
+  function (Object, constants) {
+    "use strict";
 
-  return Object.extend("com.exercise.onlinestoresapui5.model.Cart", {
-    _cart: [],
+    return Object.extend("com.exercise.onlinestoresapui5.model.Cart", {
+      _cart: [],
 
-    constructor: function () {
-      const sLocalData = localStorage.getItem(constants.localStorageDataID);
-      if (sLocalData) {
-        const oLocalData = JSON.parse(sLocalData);
-        if (oLocalData.cart) {
-          this._cart = oLocalData.cart;
+      constructor: function () {
+        const sLocalData = localStorage.getItem(constants.localStorageDataID);
+        if (sLocalData) {
+          const oLocalData = JSON.parse(sLocalData);
+          if (oLocalData.cart) {
+            this._cart = oLocalData.cart;
+          }
         }
-      }
-      return this;
-    },
+        return this;
+      },
 
-    get: function () {
-      return this._cart;
-    },
+      get: function () {
+        return this._cart;
+      },
 
-    clear: function () {
-      this._cart = [];
-    },
+      clear: function () {
+        this._cart = [];
+      },
 
-    add: function (id, quantity, price) {
-      const index = this._cart.findIndex((item) => item.id === id);
-      if (index >= 0) {
-        this._cart[index].q += quantity;
-      } else {
-        this._cart.push({ id, q: quantity, p: price });
-      }
-    },
+      add: function (id, quantity, price) {
+        const index = this._cart.findIndex((item) => item.id === id);
+        if (index >= 0) {
+          this._cart[index].q += quantity;
+        } else {
+          this._cart.push({ id, q: quantity, p: price });
+        }
+      },
 
-    drop: function (id) {
-      this._cart = this._cart.filter((item) => item.id !== id);
-    },
+      drop: function (id) {
+        this._cart = this._cart.filter((item) => item.id !== id);
+      },
 
-    replace: function (id, quantity, price) {
-      const item = this.has(id);
+      replace: function (id, quantity, price) {
+        const item = this.has(id);
 
-      if (item) {
-        const index = this._cart.indexOf(item);
-        this._cart.splice(index, 1, { id, q: quantity, p: price });
-      }
-    },
+        if (item) {
+          const index = this._cart.indexOf(item);
+          this._cart.splice(index, 1, { id, q: quantity, p: price });
+        }
+      },
 
-    has: function (id) {
-      return this._cart.find((item) => item.id === id);
-    },
+      has: function (id) {
+        return this._cart.find((item) => item.id === id);
+      },
 
-    getTotalQuantity() {
-      return this._cart.reduce((total, item) => total + item.q, 0);
-    },
+      getTotalQuantity() {
+        return this._cart.reduce((total, item) => total + item.q, 0);
+      },
 
-    getTotalAmount() {
-      return this._cart.reduce((total, item) => total + item.q * item.p, 0);
-    }
-  });
-
-});
+      getTotalAmount() {
+        return Number(
+          this._cart.reduce((total, item) => total + item.q * item.p, 0)
+        ).toFixed(2);
+      },
+    });
+  }
+);
